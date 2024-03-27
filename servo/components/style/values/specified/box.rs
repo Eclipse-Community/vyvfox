112adf790fface391e87a2ec04e846a62735acf1
K4sum1 <k4sum1@eclipse.cx>
2024-03-27 05:16:23 -0700

    Revert "Bug 1845540 - Remove some unused win-specific system colors and appearance values. r=mstange,devtools-reviewers,mhowell"
    This reverts commit 1827dc203b57541436452997261b24efd98f11e6.

--- servo/components/style/values/specified/box.rs
+++ servo/components/style/values/specified/box.rs
@@ -1640,6 +1640,14 @@ pub enum Appearance {
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
     MozSidebar,
 
+    /// Vista Rebars.
+    #[parse(condition = "ParserContext::chrome_rules_enabled")]
+    MozWinCommunicationsToolbox,
+    #[parse(condition = "ParserContext::chrome_rules_enabled")]
+    MozWinMediaToolbox,
+    #[parse(condition = "ParserContext::chrome_rules_enabled")]
+    MozWinBrowsertabbarToolbox,
+
     /// Mac help button.
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
     MozMacHelpButton,
