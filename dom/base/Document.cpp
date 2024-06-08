c069057afd157a0e7cc08fef2243d9b22870f080
K4sum1 <k4sum1@eclipse.cx>
2024-06-07 23:14:02 -0500

    Revert "Bug 1848899 - Remove widget.non-native-theme.enabled. r=desktop-theme-reviewers,dao,dholbert"
    This reverts commit 8bb4e85b36149b53add4bd49e9171767309d18eb.

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -19383,7 +19383,8 @@ void Document::AddPendingFrameStaticClone(nsFrameLoaderOwner* aElement,
 }
 
 bool Document::ShouldAvoidNativeTheme() const {
-  return !IsInChromeDocShell() || XRE_IsContentProcess();
+  return StaticPrefs::widget_non_native_theme_enabled() &&
+         (!IsInChromeDocShell() || XRE_IsContentProcess());
 }
 
 bool Document::UseRegularPrincipal() const {
