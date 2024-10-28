09faa939565a1f8266c0443579e1440bbb2fdbb8
K4sum1 <k4sum1@eclipse.cx>
2024-10-28 09:52:41 -0500

    Vista workarounds to get it running

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -1059,7 +1059,7 @@ nsresult nsWindow::Create(nsIWidget* aParent, nsNativeWidget aNativeParent,
         (aInitData->mIsPrivate) &&
         !StaticPrefs::browser_privatebrowsing_autostart();
     RefPtr<IPropertyStore> pPropStore;
-    if (!FAILED(SHGetPropertyStoreForWindow(mWnd, IID_IPropertyStore,
+    if (IsWin7OrLater() && !FAILED(SHGetPropertyStoreForWindow(mWnd, IID_IPropertyStore,
                                             getter_AddRefs(pPropStore)))) {
       PROPVARIANT pv;
       nsAutoString aumid;
